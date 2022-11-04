9b8375f0f5b0f8c2e914d752249f9c5409ace82c
Kamra <kamra.c@gmail.com>
2022-11-04 16:31:00 +0000

    task number 4 changes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 import itemsController from "./itemsController.js";
+import addPost from "./itemsController.js"
+const controller = new itemsController();
+
+controller.addPost(
+    1,
+    "Stressful Post",
+    "2022-11-04",
+    "Kamra Clemons",
+    "https://images.pexels.com/photos/1018796/pexels-photo-1018796.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "figuring this out has been difficult, but we got this!"
+  );
+  console.log(controller.content);
