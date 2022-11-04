22937ecdf69534b914fb3920ee527a1a92cf3da4
Your Name <you@example.com>
2022-11-04 15:49:37 -0500

    changes post task 5

--- js/itemsController.js
+++ js/itemsController.js
@@ -1,6 +1,6 @@
 export default class itemsController {
     constructor(currentId = 0) {
-      this.content = [];
+      this.posts = [];
       this.currentId = currentId;
     }
 
@@ -16,9 +16,18 @@ export default class itemsController {
   };
 
   //push the post to the content property
-  this.content.push(post);
+  this.posts.push(post);
   } 
 
+  loadPostsFromLocalStorage() {
+    const storagePosts = localStorage.getItem("posts")
+    if (storagePosts) {
+        const posts = JSON.parse(storagePosts)
+        for (let i = 0, size = posts.length; i < size; i++) {
+          const post = posts[i];
+          this.posts.push(post);
+        //TODO load the posts into the local posts structure (this.posts)           
+    }
 }
-
-
+}
+}
