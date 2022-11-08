3ce9747d0de9e3cf3b4aaffd543693e35e5c43cd
Lizette <Lizettemu1@gmail.com>
2022-11-08 10:21:51 -0600

    Created Dom variable to grab input / stored input into itemsController and created Event listener to display info

--- js/itemsController.js
+++ js/itemsController.js
@@ -1,33 +1,20 @@
-export default class itemsController {
-    constructor(currentId = 0) {
-      this.posts = [];
-      this.currentId = currentId;
-    }
+export default class ItemsController {
+  constructor(currentId = 0) {
+    this.postsArray = [];
+    this.currentId = currentId;
+  }
 
-  // create the addPost method 
-  addPost(postTitle, createdAt, author, imgU, text) {
-    const post = {
-      id:this.currentId++,
+  // create the addPost method
+  methodToAddPost(postTitle, firstName, image, text) {
+    const newPost = {
+      id: this.currentId++,
       postTitle: postTitle,
-      createdAt: createdAt,
-      author: author,
-      imgU: imgU,    
-      text: text
-  };
+      firstName: firstName,
+      image: image,
+      text: text,
+    };
 
-  //push the post to the content property
-  this.posts.push(post);
-  } 
-
-  loadPostsFromLocalStorage() {
-    const storagePosts = localStorage.getItem("posts")
-    if (storagePosts) {
-        const posts = JSON.parse(storagePosts)
-        for (let i = 0, size = posts.length; i < size; i++) {
-          const post = posts[i];
-          this.posts.push(post);
-        //TODO load the posts into the local posts structure (this.posts)           
-    }
-}
-}
+    //push the post to the content property
+    this.postsArray.push(newPost);
+  }
 }
