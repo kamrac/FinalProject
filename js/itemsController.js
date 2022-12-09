bbbb2725e8372ffcd28d2c878499f9620d594e84
Your Name <you@example.com>
2022-12-09 13:09:13 -0600

    form added

--- js/itemsController.js
+++ js/itemsController.js
@@ -18,3 +18,12 @@ export default class ItemsController {
     this.postsArray.push(newPost);
   }
 }
+
+document.getElementById('btn').addEventListener('click',
+function(){
+  document.querySelector('.popup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click',
+function(){
+  document.querySelector('.popup').style.display = 'none';
+})
