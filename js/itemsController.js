5fa56b7b84eff40e0d004083d9d7b9fb0a0609d1
Kamra <kamra.c@gmail.com>
2022-12-16 00:23:52 +0000

    adding fetch

--- js/itemsController.js
+++ js/itemsController.js
@@ -17,4 +17,36 @@ export default class ItemsController {
     //push the post to the content property
     this.postsArray.push(newPost);
   }
+  save(title, author, date, text, imageUrl) {
+    const data = {title, author, date, text, imageUrl };
+  
+    fetch('http://localhost:8080/post', {
+    method: 'POST', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log('Success:', data);
+    })
+    .catch((error) => {
+    console.error('Error:', error);
+    });
+  }  
+  update({title, author, date, text, imageUrl}){
+    //TODO implement this method
 }
+
+delete(postId){
+    //TODO implement this method
+}
+
+findById(postId){
+    //TODO implement this method
+}
+};
+
+
+
